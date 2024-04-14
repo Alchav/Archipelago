577c607c58beb265b3ecb277bf170c4cd01b78b9
Alchav <github@jalchavware.com>
2024-04-14 09:43:33 -0400

    Bingo silliness

--- worlds/bingo/__init__.py
+++ worlds/bingo/__init__.py
@@ -44,15 +44,29 @@ class Bingo(World):
         self.multiworld.start_hints[self.player].value.update({item for item in self.item_name_to_id})
         self.multiworld.non_local_items[self.player].value.update({item for item in self.item_name_to_id})
 
-    def create_regions(self) -> None:
+    # def create_regions(self) -> None:
+    #
+
+    # def set_rules(self) -> None:
+    #
+
+    def create_item(self, name):
+        return BingoItem(name, ItemClassification.progression, self.item_name_to_id[name], self.player)
+
+    # def create_items(self):
+    #     for item in self.item_name_to_id.keys():
+    #         self.multiworld.itempool.append(self.create_item(item))
+    #     self.multiworld.itempool += [self.create_item(item) for item in self.multiworld.random.sample(list(self.item_name_to_id.keys()), 21)]
+
+    def post_fill(self):
         menu = Region("Menu", self.player, self.multiworld)
         self.multiworld.regions.append(menu)
-        menu.locations = [Location(self.player, location_name, location_id, menu) for location_name, location_id in self.location_name_to_id.items()]
-        self.cards = [[self.multiworld.random.sample(r, 5) for r in (range(1, 16), range(16, 31), range(31, 46), range(46, 61), range(61, 76))] for _ in range(1, 9)]
+        menu.locations = [Location(self.player, location_name, location_id, menu) for location_name, location_id in
+                          self.location_name_to_id.items()]
+        self.cards = [[self.multiworld.random.sample(r, 5) for r in
+                       (range(1, 16), range(16, 31), range(31, 46), range(46, 61), range(61, 76))] for _ in range(1, 9)]
         for card in self.cards:
             card[2][2] = 0
-
-    def set_rules(self) -> None:
         for location in self.multiworld.get_region("Menu", self.player).locations:
             location.progress_type = LocationProgressType.PRIORITY
             location.item_rule = lambda item: item.game != "Bingo" and item.classification == ItemClassification.progression
@@ -73,13 +87,51 @@ class Bingo(World):
             location.access_rule = lambda state, b=bingo_calls: state.has_all(b, self.player)
             location.calls_needed = bingo_calls
 
-    def create_item(self, name):
-        return BingoItem(name, ItemClassification.progression, self.item_name_to_id[name], self.player)
+        from BaseClasses import CollectionState
+        multiworld = self.multiworld
+        def get_item_spheres():
+            state = CollectionState(multiworld)
+            locations = set(multiworld.get_filled_locations())
+            beaten_games = set()
+            while locations:
+                reachable_locations = {location for location in locations if location.can_reach(state)}
+                old_reachable_locations = None
+                while old_reachable_locations != reachable_locations:
+                    old_reachable_locations = reachable_locations.copy()
+                    reachable_events = {location for location in reachable_locations if location.address is None}
+                    for location in reachable_events:
+                        state.collect(location.item, True, location)
+                    locations -= reachable_events
+                    reachable_locations = {location for location in locations if location.can_reach(state)}
+                if not reachable_locations:
+                    break  # don't swap unreachables
+                    if locations:
+                        yield locations  # unreachable locations
+                    break
+                else:
+                    yield {loc for loc in reachable_locations if loc.player not in beaten_games and not loc.locked}
+                    beaten_games = {player for player in multiworld.player_ids if multiworld.has_beaten_game(state)}
 
-    def create_items(self):
+                for location in reachable_locations:
+                    if location.item.advancement:
+                        state.collect(location.item, True, location)
+                locations -= reachable_locations
+        item_pool = []
         for item in self.item_name_to_id.keys():
-            self.multiworld.itempool.append(self.create_item(item))
-        self.multiworld.itempool += [self.create_item(item) for item in self.multiworld.random.sample(list(self.item_name_to_id.keys()), 21)]
+            item_pool.append(self.create_item(item))
+        item_pool += [self.create_item(item) for item in self.multiworld.random.sample(list(self.item_name_to_id.keys()), 21)]
+        spheres = [s for s in get_item_spheres() if len(s) >= 96]
+        spheres.sort(key=lambda s: len(s))
+        sphere = spheres[0]
+        sphere = list(sphere)
+        sphere.sort(key=lambda l: l.name)
+        self.random.shuffle(sphere)
+        for a, b in zip(self.multiworld.get_unfilled_locations(self.player), sphere):
+            a.item = b.item
+            b.item = item_pool.pop()
+        pass
+        # for i, s in enumerate(spheres):
+        #     spheres[i] = [l for l in s if l.item.advancement]
 
     def write_spoiler(self, spoiler_handle) -> None:
         spoiler_handle.write(f'\n\nBingo Cards ({self.multiworld.player_name[self.player]}):\n')
