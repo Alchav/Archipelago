f97bceb2b8c6f782dfb9d1583f089318bc6bdeb4
alchav <alchav@jalchavware.com>
2024-12-26 03:04:24 -0500

    Extra Spire items

--- worlds/spire/__init__.py
+++ worlds/spire/__init__.py
@@ -49,13 +49,13 @@ class SpireWorld(World):
         self.multiworld.itempool += pool
 
         extra_items = []
-        for _ in range(10):
+        for _ in range(7):
             extra_items.append(SpireItem("Boss Relic", self.player))
-        for _ in range(50):
+        for _ in range(60):
             extra_items.append(SpireItem("Relic", self.player))
-        for _ in range(10):
+        for _ in range(30):
             extra_items.append(SpireItem("Rare Card Draw", self.player))
-        for _ in range(50):
+        for _ in range(30):
             extra_items.append(SpireItem("Card Draw", self.player))
         for item in extra_items:
             item.classification = ItemClassification.useful
