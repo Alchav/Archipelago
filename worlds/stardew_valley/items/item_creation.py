6d8df04a5d055f3aa8779937974949b2f048c278
alchav <alchav@jalchavware.com>
2026-02-15 00:09:57 -0500

    no sdv filler

--- worlds/stardew_valley/items/item_creation.py
+++ worlds/stardew_valley/items/item_creation.py
@@ -42,13 +42,13 @@ def create_items(item_factory: StardewItemFactory, locations_count: int, items_t
     items += unique_items
     logger.debug(f"Created {len(unique_items)} unique items")
 
-    unique_filler_items = generate_unique_filler_items(item_factory, content, options, random, locations_count - len(items))
-    items += unique_filler_items
-    logger.debug(f"Created {len(unique_filler_items)} unique filler items")
+    # unique_filler_items = generate_unique_filler_items(item_factory, content, options, random, locations_count - len(items))
+    # items += unique_filler_items
+    # logger.debug(f"Created {len(unique_filler_items)} unique filler items")
 
-    resource_pack_items = generate_resource_packs_and_traps(item_factory, options, content, random, items + items_to_exclude, locations_count - len(items))
-    items += resource_pack_items
-    logger.debug(f"Created {len(resource_pack_items)} resource packs")
+    # resource_pack_items = generate_resource_packs_and_traps(item_factory, options, content, random, items + items_to_exclude, locations_count - len(items))
+    # items += resource_pack_items
+    # logger.debug(f"Created {len(resource_pack_items)} resource packs")
 
     return items
 
